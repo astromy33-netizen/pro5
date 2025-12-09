dec7d3934401756c6f73bb280eea2b2ac4cefa87
wert1qy <astromy33@gmail.com>
2025-12-09 18:01:31 +0600

    first commit

---
+++ ts.ts
@@ -0,0 +1,14 @@
+let student:string = "Yiman"
+console.log(student)
+
+let age:Number;
+age = 16;
+console.log(age)
+let active:boolean = true;
+console.log(active)
+
+type PersonType = {
+    name:String;
+    age:16;
+    
+}
